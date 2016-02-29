518625663520531781494e0dfc994a6d2ce53b3e
Florian TIAR <contact@tiar-florian.fr>
2016-02-29 16:22:49 +0100

    Add a toolbar to MainAcivity

--- app/src/main/java/com/tiarf/wprestapitest/MainActivity.java
+++ app/src/main/java/com/tiarf/wprestapitest/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FtiarService ftiarService = new RestAdapter.Builder()
                 .setEndpoint(FtiarService.ENDPOINT)
